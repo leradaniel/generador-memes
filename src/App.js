c013cc9067677212bbb51eea7cef0e5eb83fb93d
Daniel Lera <lera.danielm@gmail.com>
2021-10-13 19:48:28 -0300

    Agregar estructura HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Selección del meme */}
+      <select>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Fry</option>
+        <option value="history">History channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart guy</option>
+      </select>
+      <br />
+      {/* Ingreso del texto */}
+      <input type="text" placeholder="Linea 1" />
+      <br />
+      <input type="text" placeholder="Linea 2" />
+      <br />
+      <button>Exportar imagen</button>
+      {/* Generar imagen y texto */}
+      <div>
+        <span>Linea 1</span>
+        <br />
+        <span>Linea 2</span>
+        <br />
+        <img src="/img/fire.jpg" alt="Fire"/>
+      </div>
     </div>
   );
 }
